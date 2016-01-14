e6bd56b3dcd41211c027cc52d1337b7a680e2a32
aceway <aceway@qq.com>
2016-01-14 14:33:28 +0800

    adjust files

--- upE3D.sh
+++ upE3D.sh
@@ -13,23 +13,21 @@ cd `dirname $0`
 if [ $# -eq 1 ];then
     if [ $1 = "o" ] || [ $1 = "official" ];then
         echo -e "Copy from official egret github"
-        cp ~/workspace/egret/egret-3d/Sample-wing/egret3d.js ./
-        cp ~/workspace/egret/egret-3d/Sample-wing/egret3d.js ./src
-        cp ~/workspace/egret/egret-3d/Sample-wing/egret3d.d.ts ./src
+        cp ~/workspace/egret/egret-3d/Sample-wing/egret3d.js ./libs/modules/egret/
+        cp ~/workspace/egret/egret-3d/Sample-wing/egret3d.d.ts ./libs/modules/egret/
     elif [ $1 = "1" ] || [ $1 = "egret3d_1" ];then
         echo -e "Copy from egret3d_1(QQ friend) github"
-        cp ~/bak/egret3d_1/Sample-wing/egret3d.js ./
-        cp ~/bak/egret3d_1/Sample-wing/egret3d.js ./src
-        cp ~/bak/egret3d_1/Sample-wing/egret3d.d.ts ./src
+        cp ~/bak/egret3d_1/Sample-wing/egret3d.js ./libs/modules/egret/
+        cp ~/bak/egret3d_1/Sample-wing/egret3d.d.ts ./libs/modules/egret/
     elif [ $1 = "m" ] || [ $1 = "my" ] || [ $1 = "mine" ];then
         echo -e "Copy from my egret github"
-        cp ~/workspace/git/github/egret-3d/Sample-wing/egret3d.js ./
-        cp ~/workspace/git/github/egret-3d/Sample-wing/egret3d.js ./src
-        cp ~/workspace/git/github/egret-3d/Sample-wing/egret3d.d.ts ./src
+        cp ~/workspace/git/github/egret-3d/Sample-wing/egret3d.js ./libs/modules/egret/
+        cp ~/workspace/git/github/egret-3d/Sample-wing/egret3d.d.ts ./libs/modules/egret/
     else
         echo -e "\n$0 [ o | 1 | m ]\n"
 		exit 0
     fi
+    cd ./libs/modules/egret/
 	uglifyjs ./egret3d.js -m -o ./egret3d.min.js
 else
     echo -e "\n$0 [ o | 1 | m ]\n"
