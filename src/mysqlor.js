fcfacf839a791feca0a2c86aa73a53b67f46cfb3
aceway <aceway@qq.com>
2016-02-04 00:34:03 +0800

    change msyql sql

--- src/mysqlor.js
+++ src/mysqlor.js
@@ -2,7 +2,7 @@ var mysql  = require('mysql');  //调用MySQL模块
 
 //创建一个connection
 var connection = mysql.createConnection({     
-  host     : '127.0.0.1',       //主机
+  host     : 'localhost',       //主机
   user     : 'findXmgr',        //MySQL认证用户名
   db       : 'DB_FINDX',        //MySQL认证用户名
   password : 'F1ndX3@r',        //MySQL认证用户密码
@@ -17,7 +17,7 @@ connection.connect(function(err){
     console.log('[connection connect]  succeed!');
 });  
 //执行SQL语句
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) { 
+connection.query('SELECT NOW() as solution', function(err, rows, fields) { 
      if (err) {
              console.log('[query] - :'+err);
         return;
